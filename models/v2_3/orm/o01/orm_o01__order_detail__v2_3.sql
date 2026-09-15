7404dfeace27c6cb5777de0ffe17c31ca19297bb
cunningham-analytics <cunninghamc.analytics@gmail.com>
2026-09-15 15:50:00 -0400

    orm 2.3 regression test

---
+++ models/v2_3/orm/o01/orm_o01__order_detail__v2_3.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+{% set args = {
+    'hierarchy_ref': 'orm_o01__hierarchy__v2_3',
+    'version': '2.3',
+    'message_type': 'ORM_O01',
+    'group': 'ORDER_DETAIL'
+} %}
+
+{{ easyhl7.parse_group(args) }}
