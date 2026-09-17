c4464cea4d4a909d91cbf5a25e475542ecdb22f9
cunningham-analytics <cunninghamc.analytics@gmail.com>
2026-09-16 20:36:21 -0400

    orm and oru 2.4 regression passing

---
+++ models/v2_4/oru/r01/oru_r01__patient__v2_4.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+{% set args = {
+    'hierarchy_ref': 'oru_r01__hierarchy__v2_4',
+    'version': '2.4',
+    'message_type': 'ORU_R01',
+    'group': 'PATIENT'
+} %}
+
+{{ easyhl7.parse_group(args) }}
