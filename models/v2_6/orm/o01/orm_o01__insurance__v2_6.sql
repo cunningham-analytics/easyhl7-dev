2701332bb5c03bb01480c6cbe1b474e7fcac2929
cunningham-analytics <cunninghamc.analytics@gmail.com>
2026-09-17 19:53:42 -0400

    fully regression tested oru r01 and orm o01

---
+++ models/v2_6/orm/o01/orm_o01__insurance__v2_6.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+{% set args = {
+    'hierarchy_ref': 'orm_o01__hierarchy__v2_6',
+    'version': '2.6',
+    'message_type': 'ORM_O01',
+    'group': 'INSURANCE'
+} %}
+
+{{ easyhl7.parse_group(args) }}
