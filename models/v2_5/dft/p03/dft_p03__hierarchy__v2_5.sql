49286bc3e7035629ba0cc0a9be228edd7ebde2be
cunningham-analytics <cunninghamc.analytics@gmail.com>
2026-09-17 21:34:45 -0400

    adding testing for dft through 2.5

---
+++ models/v2_5/dft/p03/dft_p03__hierarchy__v2_5.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+{% set args = {
+    'segment_ref': 'dft_p03__segments__v2_5',
+    'version': '2.5',
+    'message_type': 'DFT_P03'
+} %}
+
+{{ easyhl7.apply_config(args) }}
