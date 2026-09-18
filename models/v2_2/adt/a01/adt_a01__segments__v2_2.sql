68aa3d077c29deebe8c443f11758cdab89349c34
cunningham-analytics <cunninghamc.analytics@gmail.com>
2026-09-18 19:44:37 -0400

    tested adt a01

---
+++ models/v2_2/adt/a01/adt_a01__segments__v2_2.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table') }}
+
+{% set args = {
+    'message_ref': 'adt_a01_sample__v2_2',
+    'message_column': 'message'
+} %}
+
+{{ easyhl7.split_segments(args) }}
---
+++ models/v2_5_1/adt/a01/adt_a01__hierarchy__v2_5_1.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+{% set args = {
+    'segment_ref': 'adt_a01__segments__v2_5_1',
+    'version': '2.5.1',
+    'message_type': 'ADT_A01'
+} %}
+
+{{ easyhl7.apply_config(args) }}
